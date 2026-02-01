0c9da263375859d345c0fe1b96181c36c12f2887
novel16 <lapaychavez1996@gmail.com>
2026-02-01 14:07:53 +0800

    Initial commit

---
+++ database/migrations/2026_02_01_050338_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\table;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock_quantity')->default(0);
+            $table->string('image')->nullable();
+            $table->string('status')->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2026_02_01_055948_create_orders_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->string('order_number')->unique();
+
+            $table->decimal('total_amount', 10, 2);
+
+            $table->enum('status', [
+                'pending',
+                'paid',
+                'shipped',
+                'completed',
+                'cancelled'
+            ])->default('pending');
+
+            $table->enum('payment_status', [
+                'unpaid',
+                'paid',
+                'failed'
+            ])->default('unpaid');
+
+            $table->enum('payment_method', [
+                'cod',
+                'gcash',
+                'card',
+                'paypal'
+            ]);
+
+            // Guest info
+            $table->string('guest_name')->nullable();
+            $table->string('guest_email')->nullable();
+            $table->string('guest_phone')->nullable();
+
+            $table->text('shipping_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
